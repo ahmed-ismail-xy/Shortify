eded4dd53b4c08f491ed79b819f87814290247a3
Ahmed Ismail <ahmedismailxy@gmail.com>
2025-02-15 22:09:47 +0200

    Implement repository pattern for data access
    Introduce IRepository interface with CRUD methods and implement
    Repository class using ApplicationDbContext for database operations.
    Add support for asynchronous data operations and expression trees.

--- Shortify/Shortify.Domain/Abstractions/Repository/IRepository.cs
+++ Shortify/Shortify.Domain/Abstractions/Repository/IRepository.cs
@@ -1,5 +1,14 @@
-﻿namespace Shortify.Domain.Abstractions.Repository;
+﻿using System.Linq.Expressions;
+
+namespace Shortify.Domain.Abstractions.Repository;
 
 public interface IRepository<TEntity> where TEntity : Entity
 {
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity?> GetByIdAsync(Guid id);
+    IEnumerable<TEntity> GetAll();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
 }
--- Shortify/Shortify.Infrastructure/Repository.cs
+++ Shortify/Shortify.Infrastructure/Repository.cs
@@ -1,9 +1,57 @@
-﻿using Shortify.Domain.Abstractions;
+﻿using Microsoft.EntityFrameworkCore;
+using Shortify.Domain.Abstractions;
 using Shortify.Domain.Abstractions.Repository;
+using System.Linq.Expressions;
 
 namespace Shortify.Infrastructure;
 
 internal class Repository<TEntity> : IRepository<TEntity>
     where TEntity : Entity
 {
+    private readonly ApplicationDbContext _context;
+
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+    {
+        return _context.Set<TEntity>().Where(predicate);
+    }
+
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+    }
+
+    public IEnumerable<TEntity> GetAll()
+    {
+        return _context.Set<TEntity>();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        await _context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        _context.Set<TEntity>().Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(TEntity entity)
+    {
+        _context.Set<TEntity>().Remove(entity);
+        return Task.CompletedTask;
+    }
 }
