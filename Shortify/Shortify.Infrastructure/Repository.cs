80929d06cb8262f33613fdeac2e696d4cd6af7d9
Ahmed Ismail <ahmedismailxy@gmail.com>
2025-02-15 21:24:52 +0200

    Add Repository class in Shortify.Infrastructure namespace
    Introduced a new internal class `Repository<TEntity>` in the
    `Shortify.Infrastructure` namespace, implementing the
    `IRepository<TEntity>` interface with a constraint on `TEntity`
    to be of type `Entity`. Added necessary using directives for
    `Shortify.Domain.Abstractions` and `Shortify.Domain.Abstractions.Repository`.

---
+++ Shortify/Shortify.Infrastructure/Repository.cs
@@ -0,0 +1,9 @@
+﻿using Shortify.Domain.Abstractions;
+using Shortify.Domain.Abstractions.Repository;
+
+namespace Shortify.Infrastructure;
+
+internal class Repository<TEntity> : IRepository<TEntity>
+    where TEntity : Entity
+{
+}
