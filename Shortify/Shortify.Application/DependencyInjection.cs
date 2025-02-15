fcfdc181565a08c8ba84668c98823a5083fc83fd
Ahmed Ismail <ahmedismailxy@gmail.com>
2025-02-15 21:24:26 +0200

    Add DependencyInjection class and update project references
    Introduce a static class `DependencyInjection` in the `Shortify.Application` namespace with an `AddApplication` extension method for `IServiceCollection`. This method registers MediatR, AutoMapper, and FluentValidation services, configuring MediatR with logging and validation behaviors.
    
    Update `Shortify.Application.csproj` to include new package references for `AutoMapper`, `FluentValidation`, `FluentValidation.AspNetCore`, `Microsoft.Extensions.Configuration.Abstractions`, `Microsoft.Extensions.Logging.Abstractions`, and `Serilog`. Add a project reference to `Shortify.Domain\Shortify.Domain.csproj`.

---
+++ Shortify/Shortify.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shortify.Application.Abstractions.Behaviors;
+using System.Reflection;
+
+namespace Shortify.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration Configuration)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+            configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
+
+        return services;
+    }
+}
