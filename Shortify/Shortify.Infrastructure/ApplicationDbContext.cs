561c915da156cb08a34e0a3c00020ec53f9964ac
Ahmed Ismail <ahmedismailxy@gmail.com>
2025-02-15 21:24:43 +0200

    Add ApplicationDbContext class with DbContext and IUnitOfWork
    Introduce ApplicationDbContext in Shortify.Infrastructure namespace.
    - Inherits from DbContext and implements IUnitOfWork.
    - Constructor accepts DbContextOptions<ApplicationDbContext>.
    - Override OnModelCreating to apply configurations and ensure modelBuilder is not null.
    - Add necessary using directives for EF Core and domain abstractions.

---
+++ Shortify/Shortify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortify.Domain.Abstractions;
+
+namespace Shortify.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
